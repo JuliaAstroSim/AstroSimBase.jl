621a06267890a6b20bc3c854286befeae840a903
islent <leoislent@gmail.com>
2024-10-22 14:50:19 +0800

    precompile functions [skip ci]

--- src/precompile.jl
+++ src/precompile.jl
@@ -25,5 +25,12 @@
         # Functions
         emptyfunction()
         traitstring(CPU)
+
+        middle_exp([0,1,exp(1),1,0])
+        
+        findfirstzero([-1, 0, 1])
+        findzero([-1, 0, 1, 0, -1])
+        findfirstvalue([1,2,3], 2)
+        findvalue([1,2,3,2,1], 2)
     end
 end
